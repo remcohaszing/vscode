1e4f18a4791b2ad511c353b723f89d97d206d48c
Remco Haszing <remcohaszing@gmail.com>
2025-11-10 13:29:48 +0100

    Add a function to demonstrate how to get line break data

--- playground/playground.ts
+++ playground/playground.ts
@@ -3,6 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { FontMeasurements } from '../src/vs/editor/browser/config/fontMeasurements';
+import { DOMLineBreaksComputerFactory } from '../src/vs/editor/browser/view/domLineBreaksComputer';
+import { BareFontInfo } from '../src/vs/editor/common/config/fontInfo';
+import { IModelDecoration } from '../src/vs/editor/common/model';
+import { lineMetaFromDecorations } from '../src/vs/editor/common/textModelEvents';
+
 // Enable automatic dark mode for accessibility.
 const dark = matchMedia('(prefers-color-scheme: dark)');
 monaco.editor.setTheme(dark.matches ? 'vs-dark' : 'vs-light');
@@ -151,3 +157,83 @@ registerDecoratorButton('rtl', {
 	isWholeLine: true,
 	textDirection: monaco.editor.TextDirection.RTL
 });
+
+/**
+ * Create Monaco editor font info without the use of an Editor.
+ */
+function getFontInfoWithoutEditor() {
+	const bareFontInfo = BareFontInfo.createFromRawSettings({
+		// Use your own hardcoded values here.
+		fontFamily: 'arial',
+		fontSize: 14,
+		fontWeight: 'normal',
+		letterSpacing: 0,
+		lineHeight: 19,
+	}, 1, true);
+	return FontMeasurements.readFontInfo(window, bareFontInfo);
+}
+
+function getLineBreakData() {
+	// Get the font info somehow.
+	// const fontInfo = editor.getOption(monaco.editor.EditorOption.fontInfo);
+	const fontInfo = getFontInfoWithoutEditor();
+
+	// Create the same type of computer factory that’s used inside Monaco for advanced line wrapping.
+	const factory = DOMLineBreaksComputerFactory.create(window);
+	const computer = factory.createLineBreaksComputer(
+		fontInfo,
+		// We should have the model. If not, hardcode a value.
+		model.getOptions().tabSize,
+		// Other options can be retrieved from the active editor. If this isn’t available, use hardcoded values.
+		editor.getOption(monaco.editor.EditorOption.wrappingInfo).wrappingColumn,
+		editor.getOption(monaco.editor.EditorOption.wrappingIndent) as any,
+		editor.getOption(monaco.editor.EditorOption.wordBreak),
+		editor.getOption(monaco.editor.EditorOption.wrapOnEscapedLineFeeds),
+	);
+
+	// In this example we calculate line wrapping for just a couple of lines. But it is also possible to
+	// only calculate the line breaks for a smaller number of lines. For performance it may be desirable
+	// to only calculate it for lines in the viewport.
+	const lines = model.getValue().split('\n');
+	for (let index = 0; index < lines.length; index++) {
+		// Index is 0-indexed. Line numbers are 1-indexed.
+		const lineNumber = index + 1;
+		const line = lines[index];
+		const decorations = model.getLineDecorations(lineNumber);
+		const { inlineClassNames, lineInjectedTexts } = lineMetaFromDecorations(decorations as IModelDecoration[]);
+		// The order in which addRequest is called determines the order of the finalize() output.
+		// Because we add all lines sequentially in this example, the line index matches the
+		// modelLineProjectionData index below.
+		computer.addRequest(line, lineInjectedTexts, inlineClassNames, null);
+	}
+
+	const modelLineProjectionData = computer.finalize();
+
+	// We use this to store the result.
+	const viewLines: string[] = [];
+	for (let index = 0; index < lines.length; index++) {
+		const line = lines[index];
+		// The breakOffsets property of the model line projection contains the offsets as an array of numbers.
+		const breakOffsets = modelLineProjectionData[index]?.breakOffsets;
+
+		// If the model line projection is null, or the breakOffsets are empty, the line has no line breaks.
+		if (!breakOffsets?.length) {
+			viewLines.push(line);
+			continue;
+		}
+
+		let lastOffset = 0;
+		for (const offset of breakOffsets) {
+			// Slice the line into view lines according to the model line projection break offsets.
+			viewLines.push(line.slice(lastOffset, offset));
+			lastOffset = offset;
+		}
+	}
+
+	// Log a representation of the result by joining all view lines with a newline and replacing spaces
+	// with a visible placeholder.
+	console.log(viewLines.join('\n').replaceAll(' ', '·'));
+}
+
+getLineBreakData();
+window.getLineBreakData = getLineBreakData;
